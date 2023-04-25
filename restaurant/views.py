ef6e22b8c95ddaac96e28fc3ac6a1dc950ad920e
Saber Ghasempour <saber.ghasempour@gmail.com>
2023-04-25 16:34:54 +0300

    Add authentication and authorization for all endpoints

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Booking, MenuItem
 from .serializers import MenuItemSerializer, BookingSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
@@ -11,11 +12,14 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
